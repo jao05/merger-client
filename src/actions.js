a0e3009cccad9539a88b367e8144cc122f118a9f
Jesse Outlaw <JesseOutlaw03@gmail.com>
2018-12-11 13:18:54 -0500

    Add action creators related to acquisition companies

--- src/actions.js
+++ src/actions.js
@@ -3,7 +3,7 @@ import {API_BASE_URL} from '../config';
 // 1. Update profile
 
 
-// 2. Action creator to fetch companies interested in merging (async)
+// 2a. Action creator to fetch companies interested in merging (async)
 export const fetchMergerCompanies = () => dispatch => {
     fetch(`${API_BASE_URL}/userCompany`).then(res => {
         if (!res.ok) {
@@ -20,7 +20,7 @@ export const fetchMergerCompanies = () => dispatch => {
 };
 
 
-// 3. Fetch Merger Success
+// 2b. Fetch Merger Success
 export const FETCH_MERGER_COMPANIES_SUCCESS = 'FETCH_MERGER_COMPANIES_SUCCESS';
 export const fetchMergerCompaniesSuccess = companies => ({
     type: FETCH_MERGER_COMPANIES_SUCCESS,
@@ -28,26 +28,47 @@ export const fetchMergerCompaniesSuccess = companies => ({
 });
 
 
-// 4. Fetch Merger Error
+// 2c. Fetch Merger Error
 export const FETCH_MERGER_COMPANIES_ERROR = 'FETCH_MERGER_COMPANIES_ERROR';
 export const fetchMergerCompaniesError = errMsg => ({
     type: FETCH_MERGER_COMPANIES_ERROR,
     errMsg
 });
 
-/*
-// 3. Action creator to fetch companies interested in being acquired (async)
-export const fetchBoard = () => dispatch => {
-    fetch(`${API_BASE_URL}/board`).then(res => {
+
+// 3a. Action creator to fetch companies interested in being acquired (async)
+export const fetchAcquisitionCompanies = () => dispatch => {
+    fetch(`${API_BASE_URL}/userCompany`).then(res => {
         if (!res.ok) {
             return Promise.reject(res.statusText);
         }
         return res.json();
-    }).then(board => {
-        dispatch(fetchBoardSuccess(board));
+    })
+    .then(companies => {
+        dispatch(fetchAcquisitionCompaniesSuccess(companies));
+    })
+    .catch(err => {
+        dispatch(fetchAcquisitionCompaniesError(err));
     });
 };
 
+
+// 3b. Fetch Acquistion Success
+export const FETCH_ACQUISITION_COMPANIES_SUCCESS = 'FETCH_ACQUISITION_COMPANIES_SUCCESS';
+export const fetchAcquisitionCompaniesSuccess = companies => ({
+    type: FETCH_ACQUISITION_COMPANIES_SUCCESS,
+    companies
+});
+
+
+// 3c. Fetch Acquistion Error
+export const FETCH_ACQUISITION_COMPANIES_ERROR = 'FETCH_ACQUISITION_COMPANIES_ERROR';
+export const fetchAcquisitionCompaniesError = errMsg => ({
+    type: FETCH_ACQUISITION_COMPANIES_ERROR,
+    errMsg
+});
+
+/*
 // 4. Action creator to fetch companies interested in buying other companies (async)
 export const fetchBoard = () => dispatch => {
     fetch(`${API_BASE_URL}/board`).then(res => {
