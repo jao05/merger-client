95e13df9ac7000b578e0d00213e14aa6d7882168
Jesse Outlaw <JesseOutlaw03@gmail.com>
2018-12-11 19:33:02 -0500

    Add logic stubs for mergerReducer

--- src/mergerReducer.js
+++ src/mergerReducer.js
@@ -6,5 +6,130 @@ const initialState = {};
 
 export const mergerReducer = (state=initialState, action) => {
     
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
+
+    if (action.type === ADD_ITEM) {
+        return Object.assign({}, state, {
+            
+        });
+    }
+
     return state;
 };
