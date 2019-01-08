e563f6b9bc8c1eb02a37258b916ce5045501a6a7
Jesse Outlaw <JesseOutlaw03@gmail.com>
2019-01-08 13:43:52 -0500

    Style signIn component

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,9 +1,10 @@
 // The user is directed to this component if they select to 'sign-up' on the landingPage
 // After signing-up, the user is directed to the homePage
-
 import React from 'react';
 import store from '../store';
 import {createUserCompanyRequest} from '../actions';
+
+import './signUp.css';
 
 export default class SignUp extends React.Component {    
 
@@ -40,58 +41,69 @@ export default class SignUp extends React.Component {
     	return (
 	      <div>
 	        <form onSubmit={(e) => this.submitSignUp(e)}>
-	        	<label>Company Name (Username)</label>
-		        <input id='companyName'/>
+	        	<section className='companyInfo'>
+	        		<p>Company Info:</p>
+	        		<label>Company Name (Username)</label>
+			        <input id='companyName'/>
 
-		        <label>Location</label>
-		        <select id='location'>
-	              <option value="New York">New York</option>
-	              <option value="Atlanta">Atlanta</option>
-	              <option value="Memphis">Memphis</option>
-	              <option value="San Francisco">San Francisco</option>
-            	</select>
+			        <label type='password'>Password</label>
+			        <input id='password'/> 
 
-		        <label>Industry</label>
-		        <select id='industry'>
-	              <option value="Technology">Technology</option>
-	              <option value="Financial">Financial</option>
-	              <option value="Beauty">Beauty</option>
-	              <option value="Health">Health</option>
-            	</select>
+			        <label>Location</label>
+			        <select id='location'>
+		              <option value="New York">New York</option>
+		              <option value="Atlanta">Atlanta</option>
+		              <option value="Memphis">Memphis</option>
+		              <option value="San Francisco">San Francisco</option>
+	            	</select>
 
-		        <p>Contact Person:</p>
-		        <label>First Name</label>
-		        <input id='contactFirstName'/>
+			        <label>Industry</label>
+			        <select id='industry'>
+		              <option value="Technology">Technology</option>
+		              <option value="Financial">Financial</option>
+		              <option value="Beauty">Beauty</option>
+		              <option value="Health">Health</option>
+	            	</select>
+	        	</section>
+	        	
 
-		        <label>Last Name</label>
-		        <input id='contactLastName'/>
+		        <section className='contactInfo'>
+		        	<p>Contact Person:</p>
+			        <label>First Name</label>
+			        <input id='contactFirstName'/>
 
-		        <label>Email</label>
-		        <input id='contactEmail'/>
+			        <label>Last Name</label>
+			        <input id='contactLastName'/>
 
-		        <label>Open to Merger</label>
-		        <select id='openToMerger'>
-		        	<option value='true'>True</option>
-		        	<option value='false'>False</option>
-		        </select>
+			        <label>Email</label>
+			        <input id='contactEmail'/>
+		        </section>
+		        
 
-		        <label>Open to Acquisition</label>
-		        <select id='openToAcquisition'>
-		        	<option value='true'>True</option>
-		        	<option value='false'>False</option>
-		        </select>
+		        <section className='companyAttributes'>
+		        	<p>Attributes:</p>
+		        	<label>Open to Merger</label>
+			        <select id='openToMerger'>
+			        	<option value='true'>Yes</option>
+			        	<option value='false'>No</option>
+			        </select>
 
-		        <label>Open to Sell</label>
-		        <select id='openToSell'>
-		        	<option value='true'>Yes</option>
-		        	<option value='false'>No</option>
-		        </select>
+			        <label>Open to Acquisition</label>
+			        <select id='openToAcquisition'>
+			        	<option value='true'>Yes</option>
+			        	<option value='false'>No</option>
+			        </select>
 
-		        <label>Company Description</label>
-		        <input id='companyDescription'/>
+			        <label>Open to Sell</label>
+			        <select id='openToSell'>
+			        	<option value='true'>Yes</option>
+			        	<option value='false'>No</option>
+			        </select>
 
-		        <label type='password'>Password</label>
-		        <input id='password'/> 
+			        <label>Company Description</label>
+			        <textarea id='companyDescription' type='text'></textarea>			        
+		        </section>
+		        
 
 		        <button type='Submit'>Submit</button>
 	        </form>        
