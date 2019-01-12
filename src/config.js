287be5ddf0db8ae15e2de539e54df807f890fcde
Jesse Outlaw <JesseOutlaw03@gmail.com>
2019-01-11 20:07:09 -0500

    Add url for deployment

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:8000"
+    "https://boiling-tor-70749.herokuapp.com/"
 };
