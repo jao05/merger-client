9ce2921c6205427490e7d797ba47a015d8cd5233
Jesse Outlaw <JesseOutlaw03@gmail.com>
2019-01-11 20:13:36 -0500

    Correct url for production

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    "https://boiling-tor-70749.herokuapp.com/"
+    "https://boiling-tor-70749.herokuapp.com"
 };
